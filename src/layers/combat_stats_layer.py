c3161711f3394f3ac25104f22b72c1902898ad01
Woojin <hyung12379@gmail.com>
2022-03-26 12:54:47 +0900

    added dict-like stats_spec

--- src/layers/combat_stats_layer.py
+++ src/layers/combat_stats_layer.py
@@ -1,11 +1,12 @@
-class CombatStatsLayer:
-    """
-    The base layer of simlator (or another name)
-    """
-    def __init__(self):
-        self.crit = 0 # 치명
-        self.specialization = 0 # 특화
-        self.domination = 0 # 제압
-        self.swiftness = 0 # 신속
-        self.endurance = 0 # 인내
-        self.expertise = 0 # 숙련
+from jewel_rune_layer import JewelRuneLayer
+
+class CombatStatsLayer(JewelRuneLayer):
+    def __init__(self, stats_spec, jewel_spec, rune_spec):
+        super(CombatStatsLayer, self).__init__(jewel_spec, rune_spec)
+
+        self.crit = stats_spec["치명"]
+        self.specialization = stats_spec["특화"]
+        self.domination = stats_spec["제압"]
+        self.swiftness = stats_spec["신속"]
+        self.endurance = stats_spec["인내"]
+        self.expertise = stats_spec["숙련"]
