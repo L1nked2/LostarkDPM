6d2e2d0a5efc130da3aa2befbfaa76129abb6ab4
Woojin Kim <hyung12379@gmail.com>
2022-04-15 23:34:16 +0900

    :twisted_rightwards_arrows: Done issue #10 with simple print functions

--- src/layers/static/info_layer.py
+++ src/layers/static/info_layer.py
@@ -1,6 +1,16 @@
 from src.layers.static.skill_tree_layer import SkilTreeLayer
 from src.layers.utils import initialize_wrapper
 
+# we may declare check list for each layer
+# and aggregate them
+STATIC_LAYER_CHECK_LIST = [
+    "combat_stat",
+    "attack_power",
+    "engravings",
+    "artifact_set",
+    "skill_info"
+]
+
 class InfoLayer(SkilTreeLayer):
     
     @initialize_wrapper("InfoLayer", enable_start=False)
@@ -8,7 +18,13 @@ class InfoLayer(SkilTreeLayer):
         super(InfoLayer, self).__init__(**kwargs)
     
     def check_static_layer_initialization(self):
-        pass
+        for attribute_name in STATIC_LAYER_CHECK_LIST:
+            try:
+                attribute = getattr(self, attribute_name)
+            except AttributeError as e:
+                print(e)
+            else:
+                print(f"Attribute {attribute_name}: ", attribute)
 
     def print_layer_info(self, detail=True, layers=["character", "engraving", "equipment", "skiltree"]):
         for layer in layers:
