c6010aa57c904bdc74fe3adf8646938580d3535e
L1nked23 <leeerjin@gmail.com>
2022-07-03 21:47:23 +0900

    dynamic layer concept

--- src/layers/dynamic/dpm_simulator.py
+++ src/layers/dynamic/dpm_simulator.py
@@ -4,9 +4,9 @@ class DpmSimulator(BuffsLayer):
   def __init__(self, config, **kwargs):
     super(DpmSimulator, self).__init__(**kwargs)
     self.config = config
-    # ticks in seconds
-    self.tick = 0
-    self.tick_interval = 0.1
+    # timer in seconds
+    self.elapsed = 0
+    self.tick = 0.1
     
   def invoke_next_skill(self):
     pass
