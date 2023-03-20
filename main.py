9ee42f1265c1360a6f0d2f5e4b0178c5ccdbc953
L1nked23 <leeerjin@gmail.com>
2023-03-20 23:52:23 +0900

    improved main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import sys
 import csv
+from tqdm import tqdm
 from src.layers.dynamic.dpm_simulator import DpmSimulator
 from src.layers.static.character_layer import CharacterLayer
+from pyqt5.translator import translator
 from src.layers.utils import import_character
 
 
@@ -11,10 +13,12 @@ if __name__ == '__main__':
     sys.stdout = open('results.txt', 'w')
     result_csv = open('results.csv', 'w', newline='')
     csv_wrt = csv.writer(result_csv)
-    csv_wrt.writerow(['class', 'actual_dps', 'short_dps', 'long_dps', 'nuking_dps'])
+    csv_wrt.writerow(['classname_kor', 'classname_eng', 'actual_dps', 'short_dps', 'long_dps', 'nuking_dps'])
     characters_root_path = './db/characters/'
     character_file_names = os.listdir(characters_root_path)
-    for character_file_name in character_file_names:
+    translator_instance = translator()
+    for character_file_name in (progress_bar := tqdm(character_file_names)):
+      progress_bar.desc = character_file_name
       character_configs = import_character(characters_root_path + character_file_name)
       for character_config in character_configs:
         character_dict = character_config.build_dict()
@@ -23,13 +27,13 @@ if __name__ == '__main__':
         print('==========================')
         simulator.run_simulation()
         simulator.print_result()
-        result = [character_file_name]
-        result = result + simulator.get_result()
+        skill_set = os.path.basename(character_dict['skill_set'])
+        classname_kor = translator_instance.get_kor_classname(skill_set)[1]
+        classname_eng = translator_instance.get_eng_classname(skill_set)[1]
+        result = [classname_kor, classname_eng] + simulator.get_result()
         csv_wrt.writerow(result)
         print('==========================')
         simulator.print_damage_details()
         simulator.print_delay_statistics()
         simulator.print_nuking_cycle()
         print('==========================')
-
-      print(f'{character_file_names.index(character_file_name)+1}/{len(character_file_names)}, ({character_file_name}) done', file=std_out_temp)
